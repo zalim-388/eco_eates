6c62a96f7893874503ab64701732b7ce1f9e1a3d
zalim-388 <jofilx388@gmail.com>
2024-12-31 12:26:03 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/eco_eates/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.eco_eates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/SplashscreenPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Splashscreenpage extends StatefulWidget {
+  const Splashscreenpage({super.key});
+
+  @override
+  State<Splashscreenpage> createState() => _SplashscreenpageState();
+}
+
+class _SplashscreenpageState extends State<Splashscreenpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
